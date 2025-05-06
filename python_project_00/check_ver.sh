5f6f33764377e964e8042d0b021247500092d438
bsahlas <bill.sahlas@gmail.com`>
2025-05-06 09:30:55 -0400

    Fixing path to 'dist' folder.

--- python_project_00/check_ver.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
- if [ ! -f version.txt ]; then
-    echo "version.txt file not found!"
-    exit 1
-  fi
-  if ! grep -qE '^[0-9]+\.[0-9]+\.[0-9]+$' version.txt; then
-    echo "version.txt does not contain a valid version number!"
-    exit 1
-  fi
